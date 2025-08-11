a845c1785a16ea0f7bb3ee2a2da772fb2e38d6ca
ujjwal2809 <33058530+ujjwal2809@users.noreply.github.com>
2025-08-11 14:05:25 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -109,7 +109,7 @@ const Hero = () => {
                 </button>
                 
                 <button className="btn-secondary">
-                  <Play className="w-7 h-7 mr-2" />
+                  <Play className="w-5 h-5 mr-2" />
                   Watch Success Stories
                 </button>
               </div>
